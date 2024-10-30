87ef35275147cad47a90ef6e79bde3d190a8c64f
madhumitha-ravichandran26 <madhumitha26301@gmail.com>
2024-10-30 19:34:23 +0530

    first commit

---
+++ src/Components/Header.jsx
@@ -0,0 +1,15 @@
+import PropTypes from 'prop-types'
+import React, { Component } from 'react'
+
+export class Header extends Component {
+  static propTypes = {}
+
+  render() {
+    return (
+      <div></div>
+    )
+  }
+}
+
+export default Header
+
