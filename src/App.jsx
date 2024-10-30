a7142da5206a1016f17ae568756c6fff7e00f901
madhumitha-ravichandran26 <madhumitha26301@gmail.com>
2024-10-30 21:02:47 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Navbar from './Components/Navbar'
-import Header from './Components/Header'
+import Card from './Components/Card'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      
-      <Navbar></Navbar>
-      <Header></Header>
-      <h1 className="text-3xl font-bold underline"> Hello world!  </h1>
+      <Card></Card>
     </>
   )
 }
---
+++ src/Components/Card.jsx
@@ -0,0 +1,48 @@
+import React, { Component } from 'react'
+import profile from './img/profile.jpg'
+import GitHubIcon from '@mui/icons-material/GitHub';
+import PublicIcon from '@mui/icons-material/Public';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import MailIcon from '@mui/icons-material/Mail';
+
+export class Card extends Component {
+
+    icon_style={
+        color:"black",
+        fontSize:"50px"
+    }
+   
+  render() {
+    return (
+      <div>
+      <div class="flex flex-col bg-white border shadow-sm rounded-xl  w-full max-w-80 pt-50">
+      <center>
+      <img class="inline-block size-[300px] justify-around rounded-full pt-5" src={profile} alt="Avatar"/>
+      </center>
+      <div class="p-4 md:p-5">
+    <h3 class="text-lg font-bold text-black font-sans">
+      Madhumitha
+    </h3>
+    <h6 class="text-lg font-bold text-black font-sans">
+      Fullstack Developer
+    </h6>
+    <p class="mt-1 text-black font-sans">
+    Seeking an innovative and challenging career in a growing organization that provides an opportunity for career growth.
+    </p>
+       
+       <div class="flex flex-row justify-around pt-5">
+        <a href="https://www.linkedin.com/in/madhumitha263-ravichandran/"> <LinkedInIcon style={this.icon_style}></LinkedInIcon> </a>
+        <a href="https://github.com/madhumitha-r26"><GitHubIcon style={this.icon_style}></GitHubIcon></a>
+        <a href="https://madhumitha26portfolio.netlify.app/"><PublicIcon style={this.icon_style}></PublicIcon></a>
+        <a href="mailto:@madhumitharavichandran26@gmail.com"><MailIcon style={this.icon_style}></MailIcon></a>
+       </div>
+
+  </div>
+</div>
+      
+      </div>
+    )
+  }
+}
+
+export default Card
